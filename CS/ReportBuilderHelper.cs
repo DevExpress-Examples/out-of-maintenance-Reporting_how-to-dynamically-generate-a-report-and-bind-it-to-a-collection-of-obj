0b1931c64310067555037394040b3adb3aefcfa5
Vasily (DevExpress Support) <vasyaste@users.noreply.github.com>
2019-10-04 10:56:12 +0300

    Update to expressions

--- CS/ReportBuilderHelper.cs
+++ CS/ReportBuilderHelper.cs
@@ -4,8 +4,10 @@ using System.Reflection;
 using DevExpress.XtraReports.UI;
 using System.Collections.Generic;
 
-namespace K18078 {
-    public class ReportBuilderHelper {
+namespace K18078
+{
+    public class ReportBuilderHelper
+    {
         List<DataSourceDefinition> dsd;
         public XtraReport GenerateReport(List<MyComplexObject> list) {
             XtraReport r = new XtraReport();
@@ -21,24 +23,25 @@ namespace K18078 {
             InitBands(r);
             InitDetailsBasedOnXRLabel(r, dsd);
         }
-               private void InitDetailsBasedOnXRLabel(XtraReport rep, List<DataSourceDefinition> dsd) {
+        private void InitDetailsBasedOnXRLabel(XtraReport rep, List<DataSourceDefinition> dsd) {
             int colCount = dsd.Count;
-            int totalf=0;
-            for(int i = 0; i < dsd.Count; i++) 
+            int totalf = 0;
+            for (int i = 0; i < dsd.Count; i++)
                 totalf += dsd[i].Factor;
             int fWidth = (rep.PageWidth - (rep.Margins.Left + rep.Margins.Right)) / totalf;
             int incShift = 0;
-            for(int i = 0; i < colCount; i++) {
+            for (int i = 0; i < colCount; i++) {
                 XRLabel labelh = CreateLabel(dsd[i], fWidth, incShift);
                 labelh.Text = dsd[i].CaptionName;
 
                 XRLabel labeld = CreateLabel(dsd[i], fWidth, incShift);
-                labeld.DataBindings.Add("Text", null, dsd[i].Fieldname);
+                labeld.ExpressionBindings.Add(new ExpressionBinding("BeforePrint", "Text", String.Format("[{0}]", dsd[i].Fieldname)));
 
-                if(i > 0) {
+
+                if (i > 0) {
                     labelh.Borders = DevExpress.XtraPrinting.BorderSide.Right | DevExpress.XtraPrinting.BorderSide.Top | DevExpress.XtraPrinting.BorderSide.Bottom;
                     labeld.Borders = DevExpress.XtraPrinting.BorderSide.Right | DevExpress.XtraPrinting.BorderSide.Bottom;
-   
+
                 }
                 else {
                     labelh.Borders = DevExpress.XtraPrinting.BorderSide.All;
@@ -52,42 +55,46 @@ namespace K18078 {
             }
         }
 
-        private static XRLabel CreateLabel(DataSourceDefinition dsd, int fWidth, int incShift) {
-            XRLabel labeld = new XRLabel();
-            labeld.Location = new Point(incShift, 0);
-            labeld.Size = new Size(fWidth * dsd.Factor, 20);
-            return labeld;
+        private static XRLabel CreateLabel(DataSourceDefinition dsd, float fWidth, float incShift) {
+            return new XRLabel() {
+                LocationF = new PointF(incShift, 0),
+                SizeF = new SizeF(fWidth * dsd.Factor, 20)
+            };
         }
 
         private List<DataSourceDefinition> GenerateDataSourceDefinition(object myComplexObject) {
             List<DataSourceDefinition> dsdl = new List<DataSourceDefinition>();
             PropertyInfo[] pi = myComplexObject.GetType().GetProperties();
-            for(int i = 0; i < pi.Length; i++) {
+            for (int i = 0; i < pi.Length; i++) {
                 Reportable[] r = pi[i].GetCustomAttributes(typeof(Reportable), false) as Reportable[];
-                if(r.Length > 0) {
-                    DataSourceDefinition dsd = new DataSourceDefinition();
-                    dsd.CaptionName  = r[0].AlternateName == null ? pi[i].Name :  r[0].AlternateName;
-                    dsd.Fieldname = pi[i].Name;
-                    dsd.Factor = r[0].LenFactor == 0 ? 1 : r[0].LenFactor;
-                    dsdl.Add(dsd);
+                if (r.Length > 0) {
+                    dsdl.Add(new DataSourceDefinition() {
+                        CaptionName = r[0].AlternateName == null ? pi[i].Name : r[0].AlternateName,
+                        Fieldname = pi[i].Name,
+                        Factor = r[0].LenFactor == 0 ? 1 : r[0].LenFactor
+                    });
                 }
             }
-            return dsdl;      
+            return dsdl;
         }
         public void InitBands(XtraReport rep) {
             // Create bands
-            DetailBand detail = new DetailBand();
-            PageHeaderBand pageHeader = new PageHeaderBand();
-            ReportFooterBand reportFooter = new ReportFooterBand();
-            detail.Height = 20;
-            reportFooter.Height = 380;
-            pageHeader.Height = 20;
+            DetailBand detail = new DetailBand() {
+                HeightF = 20f
+            };
+            PageHeaderBand pageHeader = new PageHeaderBand() {
+                HeightF = 20f
+            };
+            ReportFooterBand reportFooter = new ReportFooterBand() {
+                HeightF = 380f
+            };
 
             // Place the bands onto a report
             rep.Bands.AddRange(new DevExpress.XtraReports.UI.Band[] { detail, pageHeader, reportFooter });
         }
     }
-    public class DataSourceDefinition {
+    public class DataSourceDefinition
+    {
         string fieldname;
 
         public string Fieldname {
@@ -106,6 +113,6 @@ namespace K18078 {
             get { return factor; }
             set { factor = value; }
         }
-    
+
     }
 }
